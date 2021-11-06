e2bc8287b5482d5e1808941aa5274a7ed1689191
Burak Hamza <thedatcal@gmail.com>
2021-11-07 02:40:13 +0300

    add user's orders to user model

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,7 +6,9 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Facades\DB;
 use Laravel\Sanctum\HasApiTokens;
+
 
 class User extends Authenticatable
 {
@@ -41,4 +43,12 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function orders(){
+        return DB::table('orders')
+            ->join('products', 'products.sku', '=', 'orders.product_sku')
+            ->select('products.name', 'products.sku')
+            ->where('user_id',$this->id)
+            ->get();
+    }
 }
