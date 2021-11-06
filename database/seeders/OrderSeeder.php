24bb21c0c28c1d86c5630f24b1180284e8792056
Burak Hamza <thedatcal@gmail.com>
2021-11-06 20:06:41 +0300

    add order model, migration and seeder

---
+++ database/seeders/OrderSeeder.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Order;
+use Illuminate\Database\Seeder;
+
+class OrderSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Order::truncate();
+
+        $csvFile = fopen(base_path('database/csv/purchased.csv'), 'r');
+        $firstLine = true;
+        while(($line = fgetcsv($csvFile)) !== false) {
+            if ($firstLine) {
+                $firstLine = false;
+                continue;
+            }
+
+            Order::create([
+                'user_id' => $line[0],
+                'product_sku' => $line[1]
+            ]);
+        }
+
+        fclose($csvFile);
+    }
+}
