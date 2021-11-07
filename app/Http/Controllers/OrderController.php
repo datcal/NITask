57dcf28a8f0bdd8c177236b6f2e403bc2344ff5d
Burak Hamza <thedatcal@gmail.com>
2021-11-07 17:45:51 +0300

    add cache forget feature to OrderController

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -3,7 +3,11 @@
 namespace App\Http\Controllers;
 
 use App\Models\Order;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Cache;
+use Symfony\Component\Routing\Matcher\RedirectableUrlMatcher;
 
 class OrderController extends Controller
 {
@@ -19,19 +23,21 @@ class OrderController extends Controller
         $order->user_id = $request->user()->id;
         $order->product_sku = $request->sku;
         $order->save();
-        return response([
-            'data' =>$order
-        ], 201);
+        Cache::forget(sprintf('%s%s',User::$USER_ORDER_CACHE_NAME,$request->user()->id));
+        return response()->json(null, Response::HTTP_NO_CONTENT);
     }
 
     /**
      * Remove the specified resource from storage.
      *
-     * @param  int  $id
+     * @param Request $request
+     * @param  string  $sku
      * @return \Illuminate\Http\Response
      */
     public function destroy(Request $request ,$sku)
     {
         Order::where('product_sku', $sku)->where('user_id',$request->user()->id)->delete();
+        Cache::forget(sprintf('%s%s',User::$USER_ORDER_CACHE_NAME,$request->user()->id));
+        return response()->json(null, Response::HTTP_NO_CONTENT);
     }
 }
